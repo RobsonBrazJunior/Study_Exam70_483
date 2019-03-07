5dd575479bd2c65a6395c0f0f56bf1a8b54f5fc2
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-03-07 11:04:01 -0400

    Scheduling different continuation tasks

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -14,12 +14,26 @@ namespace Chapter01
             Task<int> t = Task.Run(() =>
             {
                 return 42;
-            }).ContinueWith((i) =>
-            {
-                return i.Result * 2;
             });
 
-            Console.WriteLine(t.Result); // Displays 84
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+
+            Console.WriteLine(t.Result);
         }
     }
 }
