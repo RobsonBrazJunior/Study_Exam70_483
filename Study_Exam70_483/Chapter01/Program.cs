7b66aa45010daf910146569d7cde4192dc09e6ee
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-03-07 09:58:45 -0400

    Using ThreadPool

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -9,30 +9,14 @@ namespace Chapter01
 {
     public static class Program
     {
-        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
-        {
-            return Thread.CurrentThread.ManagedThreadId;
-        });
-
         public static void Main()
         {
-            new Thread(() =>
+            ThreadPool.QueueUserWorkItem((s) =>
             {
-                for (int x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("Thread A: {0}", x);
-                }
-            }).Start();
+                Console.WriteLine("Working on a thread from threadpool");
+            });
 
-            new Thread(() =>
-            {
-                for (int x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("Thread B: {0}", x);
-                }
-            }).Start();
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
