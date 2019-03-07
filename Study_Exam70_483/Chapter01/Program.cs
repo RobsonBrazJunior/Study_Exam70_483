15bd62140659e7b2a2928f3b25cc371e83f29ae0
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-03-07 10:54:51 -0400

    Adding a continuation to Task

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -14,8 +14,12 @@ namespace Chapter01
             Task<int> t = Task.Run(() =>
             {
                 return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
             });
-            Console.WriteLine(t.Result); // Displays 42
+
+            Console.WriteLine(t.Result); // Displays 84
         }
     }
 }
