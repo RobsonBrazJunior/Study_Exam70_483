bb0fd71b0cf1aba50a306cd82a030a3918762766
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-09 16:43:42 -0400

    Using a TaskFactory

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -14,13 +14,12 @@ namespace Chapter01
             Task<Int32[]> parent = Task.Run(() =>
             {
                 var results = new Int32[3];
-                new Task(() => results[0] = 0,
-                    TaskCreationOptions.AttachedToParent).Start();
-                new Task(() => results[1] = 1,
-                    TaskCreationOptions.AttachedToParent).Start();
-                new Task(() => results[2] = 2,
-                    TaskCreationOptions.AttachedToParent).Start();
-                
+
+                TaskFactory tf = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+
+                tf.StartNew(() => results[0] = 0);
+                tf.StartNew(() => results[1] = 1);
+                tf.StartNew(() => results[2] = 2);
                 return results;
             });
 
