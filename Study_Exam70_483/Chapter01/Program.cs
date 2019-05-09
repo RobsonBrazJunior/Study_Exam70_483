55f636ae3c735747782f28c3cac8607c6f442ebf
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-09 17:07:00 -0400

    Using Task.WaitAny

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -11,27 +11,23 @@ namespace Chapter01
     {
         public static void Main()
         {
-            Task[] tasks = new Task[3];
+            Task<int>[] tasks = new Task<int>[3];
 
-            tasks[0] = Task.Run(() =>
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+            while (tasks.Length > 0)
             {
-                Thread.Sleep(1000);
-                Console.WriteLine("1");
-                return 1;
-            });
-            tasks[1] = Task.Run(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("2");
-                return 2;
-            });
-            tasks[2] = Task.Run(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("3");
-                return 3;
-            });
-            Task.WaitAll(tasks);
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
         }
     }
 }
