d85bffe25d0560c020e8aefdb5973a312e22de44
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-10 09:55:42 -0400

    Using Parallel.For and Parallel.Foreach

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -11,23 +11,16 @@ namespace Chapter01
     {
         public static void Main()
         {
-            Task<int>[] tasks = new Task<int>[3];
-
-            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
-            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
-            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
-
-            while (tasks.Length > 0)
+            Parallel.For(0, 10, i =>
             {
-                int i = Task.WaitAny(tasks);
-                Task<int> completedTask = tasks[i];
+                Thread.Sleep(1000);
+            });
 
-                Console.WriteLine(completedTask.Result);
-
-                var temp = tasks.ToList();
-                temp.RemoveAt(i);
-                tasks = temp.ToArray();
-            }
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i =>
+            {
+                Thread.Sleep(1000);
+            });
         }
     }
 }
