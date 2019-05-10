4736cbde0df3b385db9033ef30da677f7e05862d
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-10 15:26:10 -0400

    Making a parallel query sequential

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -13,10 +13,11 @@ namespace Chapter01
     {
         public static void Main()
         {
-            var numbers = Enumerable.Range(0, 10);
-            var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).ToArray();
+            var numbers = Enumerable.Range(0, 20);
 
-            foreach (int i in parallelResult)
+            var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).AsSequential();
+
+            foreach (int i in parallelResult.Take(5))
                 Console.WriteLine(i);
         }
     }
