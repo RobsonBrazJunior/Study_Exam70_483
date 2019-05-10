76fac096946335f29d0c93de7ad040e3e3198f53
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-10 15:10:34 -0400

    Ordered parallel query

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -14,7 +14,7 @@ namespace Chapter01
         public static void Main()
         {
             var numbers = Enumerable.Range(0, 10);
-            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+            var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).ToArray();
 
             foreach (int i in parallelResult)
                 Console.WriteLine(i);
