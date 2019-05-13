b9e744309a733ccaacf55f06f1f711d75414fef4
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-13 09:23:37 -0400

    Enumerating a ConcurrentBag

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter01
@@ -10,16 +11,18 @@ namespace Chapter01
         {
             ConcurrentBag<int> bag = new ConcurrentBag<int>();
 
-            bag.Add(42);
-            bag.Add(21);
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(1000);
+                bag.Add(21);
+            });
 
-            int result;
-
-            if (bag.TryTake(out result))
-                Console.WriteLine(result);
-
-            if (bag.TryPeek(out result))
-                Console.WriteLine("There is a next item: {0}", result);
+            Task.Run(() =>
+            {
+                foreach (int i in bag)
+                    Console.WriteLine(i);
+            }).Wait();
         }
     }
 }
