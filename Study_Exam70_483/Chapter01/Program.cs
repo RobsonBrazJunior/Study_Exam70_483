2df86f4e7e9ea20478c62efa666eece3bd12330c
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-30 08:16:09 -0400

    A do-while loop

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -8,14 +8,11 @@ namespace Chapter01
     {
         public static void Main()
         {
-            int[] values = { 1, 2, 3, 4, 5, 6 };
-
-            int index = 0;
-            while (index < values.Length)
+            do
             {
-                Console.Write(values[index]);
-                index++;
+                Console.WriteLine("Executed once!");
             }
+            while (false);
         }
     }
 }
