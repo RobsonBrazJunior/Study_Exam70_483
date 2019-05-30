92b51c0dddd89f3d17a1be69707635e6b2060f8b
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-05-30 08:13:43 -0400

    A while loop

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -9,12 +9,12 @@ namespace Chapter01
         public static void Main()
         {
             int[] values = { 1, 2, 3, 4, 5, 6 };
-            for (int index = 0; index < values.Length; index++)
-            {
-                if (values[index] == 4)
-                    continue;
 
+            int index = 0;
+            while (index < values.Length)
+            {
                 Console.Write(values[index]);
+                index++;
             }
         }
     }
