aebd37df890b00c3cb929b4fa2d0548ba8e1e294
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-06-07 15:52:39 -0400

    Parsing an invalid number

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -4,58 +4,12 @@ using System.Linq;
 
 namespace Chapter01
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Program p = new Program();
-            p.CreateAndRaise();
-        }
-
-        public void CreateAndRaise()
-        {
-            Pub p = new Pub();
-
-            p.OnChange += (sender, e) => Console.WriteLine("Subdcriber 1 called");
-
-            p.OnChange += (sender, e) => { throw new Exception(); };
-
-            p.OnChange += (sender, e) => Console.WriteLine("Subscriber 3 called");
-
-            try
-            {
-                p.Raise();
-            }
-            catch (AggregateException ex)
-            {
-                Console.WriteLine(ex.InnerExceptions.Count);
-            }
-        }
-    }
-
-    public class Pub
-    {
-        public event EventHandler OnChange = delegate { };
-        public void Raise()
-        {
-            var exceptions = new List<Exception>();
-
-            foreach (Delegate handler in OnChange.GetInvocationList())
-            {
-                try
-                {
-                    handler.DynamicInvoke(this, EventArgs.Empty);
-                }
-                catch (Exception ex)
-                {
-                    exceptions.Add(ex);
-                }
-            }
-
-            if (exceptions.Any())
-            {
-                throw new AggregateException(exceptions);
-            }
+            string s = "NaN";
+            int i = int.Parse(s);
         }
     }
 }
