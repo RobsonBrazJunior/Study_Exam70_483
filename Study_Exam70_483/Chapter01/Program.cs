9cbb2f4c1553458505c8532b1efc5b25b5a4e75b
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2019-06-13 09:08:33 -0400

    Creating a custom exception

--- Study_Exam70_483/Chapter01/Program.cs
+++ Study_Exam70_483/Chapter01/Program.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Runtime.ExceptionServices;
-
-namespace Chapter01
+﻿namespace Chapter01
 {
     public static class Program
     {
         public static void Main()
         {
-            ExceptionDispatchInfo possibleException = null;
-
-            try
-            {
-                string s = Console.ReadLine();
-                int.Parse(s);
-            }
-            catch (FormatException ex)
-            {
-                possibleException = ExceptionDispatchInfo.Capture(ex);
-            }
-
-            if (possibleException != null)
-                possibleException.Throw();
         }
     }
 }
---
+++ Study_Exam70_483/OrderProcessingException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483
+{
+    [Serializable]
+    public class OrderProcessingException : Exception, ISerializable
+    {
+        public OrderProcessingException (int orderId)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(int orderId, string message) : base(message)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(int orderId, string message, Exception innerException) : base(message, innerException)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int)info.GetValue("OrderId", typeof(int));
+        }
+
+        public void GetObjectData (SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("OrderId", OrderId, typeof(int));
+        }
+
+        public int OrderId { get; set; }
+    }
+}
