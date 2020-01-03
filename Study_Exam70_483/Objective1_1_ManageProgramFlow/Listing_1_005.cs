e5f6fdafe86893d5c09089376238267e782cde0f
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:55:33 -0400

    Using the ThreadStaticAttribute

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_005.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using the ThreadStaticAttribute
+	public static class Listing_1_005
+	{
+		[ThreadStatic]
+		public static int _field;
+		public static void Main()
+		{
+			new Thread(() =>
+			{
+				for (int x = 0; x < 10; x++)
+				{
+					_field++;
+					Console.WriteLine("Thread A: {0}", _field);
+				}
+			}).Start();
+
+			new Thread(() =>
+			{
+				for (int x = 0; x < 10; x++)
+				{
+					_field++;
+					Console.WriteLine("Thread B: {0}", _field);
+				}
+			}).Start();
+
+			Console.ReadKey();
+		}
+	}
+}
