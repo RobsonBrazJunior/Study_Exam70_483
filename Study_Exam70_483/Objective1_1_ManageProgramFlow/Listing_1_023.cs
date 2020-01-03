c0053b1124abd9650eb25a82e04f5d23dc569f42
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:20:45 -0400

    Unordered parallel query

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_023.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Unordered parallel query
+	public static class Listing_1_023
+	{
+		public static void Main()
+		{
+			var numbers = Enumerable.Range(0, 10);
+			var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+
+			foreach (int i in parallelResult)
+				Console.WriteLine(i);
+		}
+	}
+}
