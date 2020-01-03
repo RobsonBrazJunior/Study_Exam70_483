a1da0f9f67c40a31992296f224b6eb1b9e35e359
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:40:27 -0400

    Using Parallel Language Integrated Query (PLINQ)

---
+++ Study_Exam70_483/Objective1_1/Example_1_011.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	public class Example_1_011
+	{
+		public static void Main(string[] args)
+		{
+			var aLotOfNumbers = Enumerable.Range(0, 100);
+
+			var parallelEvenNumbers = aLotOfNumbers
+				.AsParallel()
+				.AsOrdered()
+				.Where(number => number % 2 == 0)
+				.ToArray();
+
+			foreach (var evenNumber in parallelEvenNumbers)
+			{
+				Console.WriteLine(evenNumber);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
