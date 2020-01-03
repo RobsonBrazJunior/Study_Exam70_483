c49becd411c3c818e7b08bf98227aed8aa3c07b8
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:48:16 -0400

    Using a background thread

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_002.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using a background thread
+	public static class Listing_1_002
+	{
+		public static void ThreadMethod()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("ThreadProc: {0}", i);
+				Thread.Sleep(1000);
+			}
+		}
+
+		public static void Main()
+		{
+			Thread t = new Thread(new ThreadStart(ThreadMethod));
+			t.IsBackground = true;
+			t.Start();
+		}
+	}
+}
