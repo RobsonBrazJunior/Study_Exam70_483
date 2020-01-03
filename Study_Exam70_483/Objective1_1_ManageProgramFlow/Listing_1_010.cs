b400abbf5cb1fb719e124b373f7af254761c8720
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:01:51 -0400

    Adding a continuation

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_010.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Adding a continuation
+	public static class Listing_1_010
+	{
+		public static void Main()
+		{
+			Task<int> t = Task.Run(() =>
+			{
+				return 42;
+			}).ContinueWith((i) =>
+			{
+				return i.Result * 2;
+			});
+
+			Console.WriteLine(t.Result); //Displays 84
+		}
+	}
+}
