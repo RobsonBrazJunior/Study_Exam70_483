81885fa262df4fca798b45ed508f9ef3de3d74a3
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:59:44 -0400

    Starting a new Task

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_008.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Starting a new Task
+	public static class Listing_1_008
+	{
+		public static void Main()
+		{
+			Task t = Task.Run(() =>
+			{
+				for (var x = 0; x < 100; x++)
+					Console.WriteLine("*");
+			});
+
+			t.Wait();
+		}
+	}
+}
