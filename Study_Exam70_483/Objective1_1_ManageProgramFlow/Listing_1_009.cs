2343c0b1b44da0b9752e6b71ca84c0c3ae8487ce
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:00:56 -0400

    Using a Task that returns a value

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_009.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using a Task that returns a value
+	public static class Listing_1_009
+	{
+		public static void Main()
+		{
+			Task<int> t = Task.Run(() =>
+			{
+				return 42;
+			});
+
+			Console.WriteLine(t.Result); //Displays 42
+		}
+	}
+}
