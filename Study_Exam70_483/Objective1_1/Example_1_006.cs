029ae02be4dd5d6730c74e39b0f7d0eb7388aa83
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:29:35 -0400

    MultiThreading

---
+++ Study_Exam70_483/Objective1_1/Example_1_006.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//MultiThreading
+	public class Example_1_006
+	{
+		public static void Main(string[] args)
+		{
+			Task<string> thePowerfulTaskWithReturn = Task
+				.Run(() => "Junior is")
+				.ContinueWith((firstResult) => firstResult.Result + " sleepy!");
+
+			Console.WriteLine(thePowerfulTaskWithReturn.Result);
+			Console.ReadLine();
+		}
+	}
+}
