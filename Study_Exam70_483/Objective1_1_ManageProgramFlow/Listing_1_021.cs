7ace61a57b6927fe0c5856a34a64a09f1637a7f2
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:18:25 -0400

    Continuing on a thread pool instead of the UI thread

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_021.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Continuing on a thread pool instead of the UI thread
+	public class Listing_1_021
+	{
+		//private async void Button_Click(object sender, RoutedEventArgs e)
+		//{
+		//	HttpClient httpClient = new HttpClient();
+
+		//	string content = await httpClient.GetStringAsync("http://www.microsoft.com").ConfigureAwait(false);
+
+		//	using (FileStream sourceStream = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None, 4096, useAsync: true))
+		//	{
+		//		byte[] encodedText = Encoding.Unicode.GetBytes(content);
+		//		await sourceStream.WriteAsync(encodedText, 0, encodedText.Length).ConfigureAwait(false);
+		//	};
+		//}
+	}
+}
