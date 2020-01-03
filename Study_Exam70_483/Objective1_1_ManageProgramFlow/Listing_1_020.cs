2041d76f6879e5ad878a93fe3fd1f44afb3ca59f
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:17:36 -0400

    Using ConfigureAwait

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_020.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using ConfigureAwait
+	public class Listing_1_020
+	{
+		public static void Main()
+		{
+
+		}
+
+		//private async void Button_Click(object sender, RoutedEventArgs e)
+		//{
+		//	HttpClient httpClient = new HttpClient();
+
+		//	string content = await httpClient.GetStringAsync("http://www.microsft.com").ConfigureAwait(false);
+
+		//	Output.Content = content;
+		//}
+	}
+}
