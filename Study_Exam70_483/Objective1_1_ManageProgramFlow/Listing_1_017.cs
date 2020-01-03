1c0da762500ecbad3cfcb3cf0f85a69e2ed3236a
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:13:17 -0400

    Using Parallel.Break

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_017.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using Parallel.Break
+	public static class Listing_1_017
+	{
+		public static void Main()
+		{
+			ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+			{
+				if (i == 500)
+				{
+					Console.WriteLine("Breaking loop");
+					loopState.Break();
+				}
+
+				return;
+			});
+		}
+	}
+}
