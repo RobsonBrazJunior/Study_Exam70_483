7984cc40c2bb9e0660fe8888cdbf0199f7c4796d
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:31:34 -0400

    MultiThreading

---
+++ Study_Exam70_483/Objective1_1/Example_1_007.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//MultiThreading
+	public class Example_1_007
+	{
+		public static void Main(string[] args)
+		{
+			Stopwatch myStopwatch = new Stopwatch();
+
+			Task[] aSetOfTasks = new Task[3];
+
+			myStopwatch.Start();
+			aSetOfTasks[0] = Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+				Console.WriteLine("Junior drinks coffee.");
+			});
+
+			aSetOfTasks[1] = Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+				Console.WriteLine("Junior fixes some bugs.");
+			});
+
+			aSetOfTasks[2] = Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+				Console.WriteLine("Junior... falls asleep");
+			});
+
+			Task.WaitAll(aSetOfTasks);
+
+			myStopwatch.Stop();
+			var timeStampStopWatch = myStopwatch.Elapsed;
+			Console.WriteLine($"It took {timeStampStopWatch.ToString("g")}");
+
+			Console.ReadLine();
+		}
+	}
+}
