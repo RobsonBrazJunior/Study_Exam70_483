2bbedebc847064e93e8464d1dfb34cd88f0407c0
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:26:20 -0400

    UsingThreadPools

---
+++ Study_Exam70_483/Objective1_1/Example_1_004.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//UsingThreadPools
+	public class Example_1_004
+	{
+		public static void Main(string[] args)
+		{
+			ThreadPool.QueueUserWorkItem((work) =>
+			{
+				Console.WriteLine("Junior is trying to do something here...");
+			});
+
+			Console.ReadLine();
+		}
+	}
+}
