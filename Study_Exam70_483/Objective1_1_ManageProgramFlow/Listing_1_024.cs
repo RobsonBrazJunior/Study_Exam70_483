c92798c4693a187d3f226b50b10942f97d2066e0
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:22:50 -0400

    Ordered parallel query

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_024.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Ordered parallel query
+	public static class Listing_1_024
+	{
+		public static void Main()
+		{
+			//var numbers = Enumerable.Range(0, 10);
+			//var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).ToArray();
+
+			var parallelResult = Enumerable.Range(0, 10).AsParallel().AsOrdered().Where(i => i % 2 == 0).ToArray();
+
+			foreach (int i in parallelResult)
+				Console.WriteLine(i);
+		}
+	}
+}
