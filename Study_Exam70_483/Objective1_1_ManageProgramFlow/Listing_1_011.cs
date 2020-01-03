4ed38097fa0192eb51f06f672a9ab0986763c662
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:02:49 -0400

    Scheduling different continuation tasks

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_011.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Scheduling different continuation tasks
+	public static class Listing_1_011
+	{
+		public static void Main()
+		{
+			Task<int> t = Task.Run(() =>
+			{
+				return 42;
+			});
+
+			t.ContinueWith((i) =>
+			{
+				Console.WriteLine("Canceled");
+			}, TaskContinuationOptions.OnlyOnCanceled);
+
+			t.ContinueWith((i) =>
+			{
+				Console.WriteLine("Faulted");
+			}, TaskContinuationOptions.OnlyOnFaulted);
+
+			var completedTask = t.ContinueWith((i) =>
+			{
+				Console.WriteLine("Completed");
+			}, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+			completedTask.Wait();
+		}
+	}
+}
