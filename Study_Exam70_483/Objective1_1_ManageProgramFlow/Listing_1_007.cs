4d00a0f46fc218e817b3f6d6bef353ed33786a82
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:58:51 -0400

    Queuing some work to the thread pool

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_007.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Queuing some work to the thread pool
+	public static class Listing_1_007
+	{
+		public static void Main()
+		{
+			ThreadPool.QueueUserWorkItem((s) =>
+			{
+				Console.WriteLine("Working on a thread from threadpool");
+			});
+
+			Console.ReadLine();
+		}
+	}
+}
