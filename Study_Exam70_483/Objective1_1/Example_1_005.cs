436a07c5a55625e885de017c40c530eb6dd8eae4
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:28:10 -0400

    Tasks

---
+++ Study_Exam70_483/Objective1_1/Example_1_005.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//Tasks
+	public class Example_1_005
+	{
+		public static void Main(string[] args)
+		{
+			Task thePowerfulTask = Task.Run(() =>
+			{
+				for (var i = 0; i < 1000; i++)
+					Console.WriteLine("for Execution nº: " + i + ", ");
+			});
+
+			thePowerfulTask.Wait();
+
+			Task<string> thePowerfulTaskWithReturn = Task.Run(() => "sleepy!");
+
+			Console.WriteLine("Junior is " + thePowerfulTaskWithReturn.Result);
+			Console.ReadLine();
+		}
+	}
+}
