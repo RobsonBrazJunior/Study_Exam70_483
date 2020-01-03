cf051d5a8a8a7a03532526c06a9dc93ae9f8e0d7
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:16:50 -0400

    Scalability versus responsiveness

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_019.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Scalability versus responsiveness
+	public class Listing_1_019
+	{
+		public static void Main() { }
+
+		public Task SleepAsyncA(int millisecondsTimeout)
+		{
+			return Task.Run(() => Thread.Sleep(millisecondsTimeout));
+		}
+
+		public Task SleepAsyncB(int millisecondsTimeout)
+		{
+			TaskCompletionSource<bool> tcs = null;
+			var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+			tcs = new TaskCompletionSource<bool>(t);
+			t.Change(millisecondsTimeout, -1);
+			return tcs.Task;
+		}
+	}
+}
