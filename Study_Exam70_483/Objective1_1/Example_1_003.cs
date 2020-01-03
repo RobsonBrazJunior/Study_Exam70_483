5757bf7fb210b063842fa9cb599af41aa2cb31f9
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:25:01 -0400

    ParametrizedThreadStart

---
+++ Study_Exam70_483/Objective1_1/Example_1_003.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//ParametrizedThreadStart
+	public class Example_1_003
+	{
+		public static void UltraCoolMethod(object o)
+		{
+			for (var i = 0; i < (int)o; i++)
+			{
+				Console.WriteLine($"Execution Thread Nº {i}");
+				Thread.Sleep(0);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			Thread theThread = new Thread(new ParameterizedThreadStart(UltraCoolMethod));
+			//You can do it implicitly as well
+			//Thread theThread = new Thread(UltraCoolMethod)
+			theThread.Start(10);
+
+			theThread.Join();
+		}
+	}
+}
