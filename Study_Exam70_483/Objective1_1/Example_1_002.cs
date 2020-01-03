db85f83ae16598c6cb3be9ae866237dc40a4aa9d
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 20:21:11 -0400

    MultiThreading

---
+++ Study_Exam70_483/Objective1_1/Example_1_002.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Study_Exam70_483.Objective1_1
+{
+	//MultiThreading
+	public class Example_1_002
+	{
+		public static void UltraCoolMethod(object o)
+		{
+			const int iterationNumber = 10;
+			for (var i = 0; i < iterationNumber; i++)
+			{
+				Console.WriteLine($"Execution Thread Nº {i}");
+				Thread.Sleep(0);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			Thread theThread = new Thread(UltraCoolMethod);
+			theThread.IsBackground = true;
+
+			theThread.Start();
+		}
+	}
+}
