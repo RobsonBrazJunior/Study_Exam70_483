c3ad63e64e6977a6a1e2a24de3263338180cbde6
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-02 21:14:39 -0400

    Async and Await

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_018.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Async and Await
+	public static class Listing_1_018
+	{
+		public static void Main()
+		{
+			string result = DownloadContent().Result;
+			Console.WriteLine(result);
+		}
+
+		public static async Task<string> DownloadContent()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				string result = await client.GetStringAsync("http://www.microsoft.com");
+				return result;
+			}
+		}
+	}
+}
