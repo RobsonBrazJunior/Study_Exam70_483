1470b3259ec34bc2928ed8672fee38fa1dae49e7
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-03 20:39:22 -0400

    Using ForAll

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_026.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using ForAll
+	public class Listing_1_026
+	{
+		public static void Main()
+		{
+			var numbers = Enumerable.Range(0, 20);
+
+			var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0);
+
+			parallelResult.ForAll(e => Console.WriteLine(e));
+		}
+
+	}
+}
