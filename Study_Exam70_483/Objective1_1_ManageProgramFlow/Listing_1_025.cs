84848316754fa54a04daceb56aa8a099a44a2cf6
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-03 20:26:28 -0400

    Making a parallel query sequential

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_025.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Making a parallel query sequential
+	public class Listing_1_025
+	{
+		public static void Main()
+		{
+			var numbers = Enumerable.Range(0, 20);
+			var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).AsSequential();
+
+			foreach (int i in parallelResult.Take(5))
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
