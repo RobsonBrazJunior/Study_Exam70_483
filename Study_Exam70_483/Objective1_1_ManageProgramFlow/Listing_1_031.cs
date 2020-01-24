d594a327d0647b7b78630fb7ab5fbb04ebefe2b0
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:17:10 -0400

    Using ConcurrentBag

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_031.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	public static class Listing_1_031
+	{
+		public static void Main()
+		{
+			ConcurrentBag<int> bag = new ConcurrentBag<int>();
+			Task.Run(() =>
+			{
+				bag.Add(42);
+				Thread.Sleep(1000);
+				bag.Add(21);
+			});
+
+			Task.Run(() =>
+			{
+				foreach (int i in bag)
+					Console.WriteLine(i);
+			}).Wait();
+		}
+	}
+}
