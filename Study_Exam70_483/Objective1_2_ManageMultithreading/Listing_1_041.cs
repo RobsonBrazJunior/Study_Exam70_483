e1436397cb1d597ac2d6a3537f45840a75fa24d2
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:55:31 -0400

    Compare and exchange as a nonatomic operation

---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_041.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	//Compare and exchange as a nonatomic operation
+	public static class Listing_1_041
+	{
+		static int value = 1;
+
+		public static void Main()
+		{
+			Task t1 = Task.Run(() =>
+			{
+				if (value == 1)
+				{
+					//Removing the following line will change de output
+					Thread.Sleep(1000);
+					value = 2;
+				}
+			});
+
+			Task t2 = Task.Run(() =>
+			{
+				value = 3;
+			});
+
+			Task.WaitAll(t1, t2);
+			Console.WriteLine(value);  //Displyas 2
+		}
+	}
+}
