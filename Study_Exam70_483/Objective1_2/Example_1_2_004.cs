099b4b6069d8899611ae5ab662bd25124e1840a7
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:35:00 -0400

    Example_1_2_004

---
+++ Study_Exam70_483/Objective1_2/Example_1_2_004.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2
+{
+	class Example_1_2_004
+	{
+		private static int _theFlag;
+		private static volatile int _theValue;
+
+		static void Main(string[] agrs)
+		{
+			TheFirstThread();
+			TheSecondThread();
+		}
+
+		public static void TheFirstThread()
+		{
+			_theValue = 10;
+			_theFlag = 1;
+		}
+
+		public static void TheSecondThread()
+		{
+			if (_theFlag == 1)
+				Console.WriteLine(_theValue);
+		}
+	}
+}
