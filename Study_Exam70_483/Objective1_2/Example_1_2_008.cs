b548ddc206179865cfc507a6ad3197df229f91bf
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:41:09 -0400

    Example_1_2_008

---
+++ Study_Exam70_483/Objective1_2/Example_1_2_008.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2
+{
+	class Example_1_2_008
+	{
+		static void Main(string[] args)
+		{
+			Task notThaPowerfulTask = Task.Run(() =>
+			{
+				Console.WriteLine("A long-running task is executed right now...");
+				Thread.Sleep(10000);
+			});
+
+			Console.WriteLine("Well, I have only 5s...");
+			int result = Task.WaitAny(new[] { notThaPowerfulTask }, 5000);
+
+			if (result == -1)
+				Console.WriteLine("I am out... It is taking too much time!");
+
+			Console.ReadLine();
+		}
+	}
+}
