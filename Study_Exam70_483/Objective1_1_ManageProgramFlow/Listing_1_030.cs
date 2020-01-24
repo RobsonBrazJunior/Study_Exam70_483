dc1a46c1ebc99e9375d17fa9f6222a3baf2de283
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:15:06 -0400

    Using a ConcurrentBag

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_030.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using a ConcurrentBag
+	public static class Listing_1_030
+	{
+		public static void Main()
+		{
+			ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+			bag.Add(42);
+			bag.Add(21);
+
+			int result;
+			if (bag.TryTake(out result))
+				Console.WriteLine(result);
+
+			if (bag.TryTake(out result))
+				Console.WriteLine("There is a next item: {0}", result);
+		}
+	}
+}
