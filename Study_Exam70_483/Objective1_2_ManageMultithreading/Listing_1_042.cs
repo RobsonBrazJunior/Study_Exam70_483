a82aa6f35e5ca089e31150163cd4bf1e48880738
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:56:55 -0400

    Using a CancellationToken

---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_042.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	//Using a CancellationToken
+	public class Listing_1_042
+	{
+		public static void Main()
+		{
+			CancellationTokenSource cancellationTokenSoruce = new CancellationTokenSource();
+			CancellationToken token = cancellationTokenSoruce.Token;
+
+			Task task = Task.Run(() =>
+			{
+				while (!token.IsCancellationRequested)
+				{
+					Console.WriteLine("*");
+					Thread.Sleep(1000);
+				}
+			}, token);
+
+			Console.WriteLine("Press enter to stop the task");
+			Console.ReadLine();
+			cancellationTokenSoruce.Cancel();
+
+			Console.WriteLine("Press enter to end the application");
+			Console.ReadLine();
+		}
+	}
+}
