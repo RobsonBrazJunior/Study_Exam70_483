64ac28ccf8bf214e0d414cc77a31050883ec086d
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:37:17 -0400

    Example_1_2_005

--- Study_Exam70_483/Objective1_2/Example_1_2_004.cs
+++ Study_Exam70_483/Objective1_2/Example_1_2_004.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Study_Exam70_483.Objective1_2
 {
---
+++ Study_Exam70_483/Objective1_2/Example_1_2_005.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2
+{
+	class Example_1_2_005
+	{
+		static void Main(string[] args)
+		{
+			int theAlmightyZero = 0;
+			object _lockTheSavior = new object();
+
+			Task newAmazingTask = Task.Run(() =>
+			{
+				for (int i = 0; i < 1000000; i++)
+					Interlocked.Increment(ref theAlmightyZero);
+			});
+
+			for (int j = 0; j < 1000000; j++)
+				Interlocked.Decrement(ref theAlmightyZero);
+
+			newAmazingTask.Wait();
+
+			Console.WriteLine("Here it comes a ZERO value (that is STILL true)! " + theAlmightyZero);
+			Console.ReadLine();
+		}
+	}
+}
