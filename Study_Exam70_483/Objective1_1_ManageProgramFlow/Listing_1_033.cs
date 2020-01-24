0994577d1baf4411097d4ae43e7e9f5d60d43c92
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:20:53 -0400

    Using a Queue

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_033.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	public static class Listing_1_033
+	{
+		public static void Main()
+		{
+			ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+			queue.Enqueue(42);
+
+			int result;
+			if (queue.TryDequeue(out result))
+				Console.WriteLine("Dequeued: {0}", result);
+		}
+	}
+}
