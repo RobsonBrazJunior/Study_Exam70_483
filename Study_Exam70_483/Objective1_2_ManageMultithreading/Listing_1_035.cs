0c6c88a3f2261f8997552f5ca4d295f2fb03acf8
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:44:33 -0400

    Accessing shared data in a multithreaded application

---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_035.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	//Accessing shared data in a multithreaded application
+	public class Listing_1_035
+	{
+		static void Main()
+		{
+			int n = 0;
+
+			var up = Task.Run(() =>
+			{
+				for (int i = 0; i < 1000000; i++)
+					n++;
+			});
+
+			for (int i = 0; i < 1000000; i++)
+				n--;
+
+			up.Wait();
+			Console.WriteLine(n);
+		}
+	}
+}
