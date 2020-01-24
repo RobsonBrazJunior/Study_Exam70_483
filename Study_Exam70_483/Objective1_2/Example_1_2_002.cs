d2ae2a2c793499ebd1e91ba275eee49c1be1274b
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:31:00 -0400

    Example_1_2_002

---
+++ Study_Exam70_483/Objective1_2/Example_1_2_002.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2
+{
+	class Example_1_2_002
+	{
+		static void Main(string[] args)
+		{
+			int theAlmightyZero = 0;
+			object _lockTheSavior = new object();
+
+			Task newAmazingTask = Task.Run(() =>
+			{
+				for (int i = 0; i < 1000000; i++)
+					lock (_lockTheSavior)
+						theAlmightyZero++;
+			});
+
+			for (int j = 0; j < 1000000; j++)
+				lock (_lockTheSavior)
+					theAlmightyZero--;
+
+			newAmazingTask.Wait();
+
+			Console.WriteLine("Here it comes a ZERO value (now that is true)! " + theAlmightyZero);
+		}
+	}
+}
