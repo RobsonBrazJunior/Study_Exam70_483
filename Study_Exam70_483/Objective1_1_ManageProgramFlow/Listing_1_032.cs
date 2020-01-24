acdfd5b25cb3564de2fdbdb9885c992097dae921
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:19:03 -0400

    Using a ConcurrentStack

---
+++ Study_Exam70_483/Objective1_1_ManageProgramFlow/Listing_1_032.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Study_Exam70_483.Objective1_1_ManageProgramFlow
+{
+	//Using a ConcurrentStack
+	public static class Listing_1_032
+	{
+		public static void Main()
+		{
+			ConcurrentStack<int> stack = new ConcurrentStack<int>();
+
+			stack.Push(42);
+
+			int result;
+			if (stack.TryPop(out result))
+				//Console.WriteLine(("Popped: {0}", result)); === Ao utilizar dois pares de parenteses, ele apresenta o contexto explicitamente exceto o valor de variável
+				Console.WriteLine("Popped: {0}", result);
+
+			stack.PushRange(new int[] { 1, 2, 3 });
+
+			int[] values = new int[2];
+			stack.TryPopRange(values);
+
+			foreach (int i in values)
+				Console.WriteLine(i);
+		}
+	}
+}
