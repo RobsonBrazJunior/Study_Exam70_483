8b925ad4c9a00c3c5e1c09d73cf9cd7e82884b1f
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-23 21:52:54 -0400

    A potential problem with multithreaded code

--- Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_038.cs
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_038.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
 
 namespace Study_Exam70_483.Objective1_2_ManageMultithreading
 {
---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_039.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	//A potential problem with multithreaded code
+	class Listing_1_039
+	{
+		private static volatile int _flag = 0;
+		private static volatile int _value = 0;
+
+		static void Main()
+		{
+		}
+
+		public static void Thread1()
+		{
+			_value = 5;
+			_flag = 1;
+		}
+
+		public static void Thread2()
+		{
+			if (_flag == 1)
+				Console.WriteLine(_value);
+		}
+	}
+}
