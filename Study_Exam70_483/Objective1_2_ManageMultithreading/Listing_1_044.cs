6d5733e8d0bbca518d64e5f5a993616741fc36b2
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-28 20:57:13 -0400

    Adding a continuation for canceled tasks

---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_044.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	//Adding a continuation for canceled tasks
+	class Listing_1_044
+	{
+		static void Main()
+		{
+			CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+			CancellationToken token = cancellationTokenSource.Token;
+
+			Task task = Task.Run(() =>
+			{
+				while (!token.IsCancellationRequested)
+				{
+					Console.Write("*");
+					Thread.Sleep(1000);
+				}
+			}, token).ContinueWith((t) =>
+			{
+				t.Exception.Handle((e) => true);
+				Console.WriteLine("You have canceled the task");
+			}, TaskContinuationOptions.OnlyOnCanceled);
+
+			try
+			{
+				Console.WriteLine("Press enter to stop the task");
+				Console.ReadLine();
+
+				cancellationTokenSource.Cancel();
+				task.Wait();
+			}
+			catch (AggregateException e)
+			{
+				Console.WriteLine(e.InnerExceptions[0].Message);
+			}
+			Console.WriteLine("Press enter to end the application");
+			Console.ReadLine();
+		}
+	}
+}
