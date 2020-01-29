89be04f518d3ca03b3e9f893a47d1eb2c230eb0b
Junior Braz <robsonbrazjunior@gmail.com>
2020-01-28 21:35:29 -0400

    Listing_1_045

---
+++ Study_Exam70_483/Objective1_2_ManageMultithreading/Listing_1_045.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study_Exam70_483.Objective1_2_ManageMultithreading
+{
+	class Listing_1_045
+	{
+		public static void Main()
+		{
+			Task longRunning = Task.Run(() =>
+			{
+				Thread.Sleep(10000);
+			});
+
+			int index = Task.WaitAny(new[] { longRunning }, 1000);
+
+			if (index == -1)
+				Console.WriteLine("Task timed out");
+		}
+	}
+}
